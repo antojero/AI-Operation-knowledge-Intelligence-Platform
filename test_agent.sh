8f38cef8e5a897337c7ce0f352386d61be65f296
Anto Jero <92205426+antojero@users.noreply.github.com>
2026-01-29 00:04:30 +0530

    Delete test_agent.sh

--- test_agent.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Triggering Agent Run..."
-curl -X POST http://localhost:8001/agent/run \
-  -H "Content-Type: application/json" \
-  -d '{"task": "What is the latest version of LangGraph?"}'
